c68aaba44c5944cfa22e09340837a289f8958649
Khalid Evan <khalidevanthirafi@gmail.com>
2024-05-07 22:02:34 +0700

    Branch Hello World Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World is Normals!");
+        System.out.println("Hello World is Normal!");
     }
 }

aa9b66a04e75477189b8f424b0ee41210bfd9898
Khalid Evan <khalidevanthirafi@gmail.com>
2024-05-07 22:01:18 +0700

    Updated Commit Demo Jayjay

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World is Normal!");
+        System.out.println("Hello World is Normals!");
     }
 }
